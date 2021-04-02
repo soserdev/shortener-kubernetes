c00d2a6554e2f680bf7e55b9bd4d51eab0ca4f89
Simon Oser <sqeebs@gmail.com>
2021-04-02 13:03:02 +0200

    Fixes short url button

--- jumper-frontend/scripts/index.js
+++ jumper-frontend/scripts/index.js
@@ -1,4 +1,4 @@
-document.querySelector('#url-form').addEventListener('submit', function(e) {
+document.querySelector('#shorten-form').addEventListener('submit', function(e) {
     e.preventDefault()
     console.log(e.target.elements.originalUrl.value)
     e.target.elements.originalUrl.value = ''
